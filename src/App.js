4e6e59ee65f374a89ede19e441ca485ad375299e
Jonathanbir <jonathan79111@gmail.com>
2023-01-20 13:42:38 +0800

    簡易購物車: 相同品項累加

--- src/App.js
+++ src/App.js
@@ -11,10 +11,19 @@ function App() {
   const cartReducer = useReducer(
     (state, action) => {
       const cartList = [...state.cartList];
+      // #1 先取得當前購物車目標品項索引
+      const index = cartList.findIndex((item) => item.id === action.payload.id);
+      console.log(index);
       console.log(action);
       switch (action.type) {
         case "ADD_TO_CART":
-          cartList.push(action.payload);
+          if (index === -1) {
+            //還未加入到購物車內
+            cartList.push(action.payload);
+          } else {
+            //當前購物車的項目與加入的項目一致
+            cartList[index].quantity += action.payload.quantity;
+          }
           return {
             ...state,
             cartList,
