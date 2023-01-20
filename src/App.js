e830d3ae865d141ab63a5f3475fe706bae8a661c
Jonathanbir <jonathan79111@gmail.com>
2023-01-20 15:00:47 +0800

    完成購物車

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useReducer } from "react";
-import { CartContext } from "./store";
+import { CartContext, cartReducer, cartInit } from "./store";
 
 //外部套件
 import Navbar from "./components/Navbar";
@@ -8,59 +8,10 @@ import Cart from "./components/Cart";
 import "./assets/scss/all.scss";
 
 function App() {
-  const cartReducer = useReducer(
-    (state, action) => {
-      const cartList = [...state.cartList];
-      // #1 先取得當前購物車目標品項索引
-      const index = cartList.findIndex((item) => item.id === action.payload.id);
-      console.log(index);
-      console.log(action);
-      switch (action.type) {
-        case "ADD_TO_CART":
-          if (index === -1) {
-            //還未加入到購物車內
-            cartList.push(action.payload);
-          } else {
-            //當前購物車的項目與加入的項目一致
-            cartList[index].quantity += action.payload.quantity;
-          }
-          // const array = cartList.map((item) => {
-          //   return item.quantity * item.price;
-          // });
-          // const total = array.reduce((a, b) => {
-          //   return a + b;
-          // }, 0);
-          //reduce
-          return {
-            ...state,
-            cartList,
-            total: calculateTotalPrice(cartList),
-          };
-        case "CHANGE_CART_QUANTITY":
-          cartList[index].quantity = action.payload.quantity;
-          return {
-            ...state,
-            cartList,
-            total: calculateTotalPrice(cartList),
-          };
-        case "REMOVE_CART_ITEM":
-          cartList.splice(index, 1);
-          return {
-            ...state,
-            cartList,
-            total: calculateTotalPrice(cartList),
-          };
-        default:
-          return state;
-      }
-    },
-    {
-      cartList: [],
-    }
-  );
+  const reducer = useReducer(cartReducer, cartInit);
 
   return (
-    <CartContext.Provider value={cartReducer}>
+    <CartContext.Provider value={reducer}>
       <Navbar />
       <div className="container mt-4">
         {/*外層隔線*/}
@@ -76,13 +27,5 @@ function App() {
       </div>
     </CartContext.Provider>
   );
-}
-
-function calculateTotalPrice(cartList) {
-  return cartList
-    .map((item) => {
-      return item.quantity * item.price;
-    })
-    .reduce((a, b) => a + b, 0);
 }
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,3 +1,61 @@
 import { createContext } from "react";
 
+function calculateTotalPrice(cartList) {
+  return cartList
+    .map((item) => {
+      return item.quantity * item.price;
+    })
+    .reduce((a, b) => a + b, 0);
+}
+
+export const cartInit = {
+  cartList: [],
+};
+
+export const cartReducer = (state, action) => {
+  const cartList = [...state.cartList];
+  // #1 先取得當前購物車目標品項索引
+  const index = cartList.findIndex((item) => item.id === action.payload.id);
+  console.log(index);
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TO_CART":
+      if (index === -1) {
+        //還未加入到購物車內
+        cartList.push(action.payload);
+      } else {
+        //當前購物車的項目與加入的項目一致
+        cartList[index].quantity += action.payload.quantity;
+      }
+      // const array = cartList.map((item) => {
+      //   return item.quantity * item.price;
+      // });
+      // const total = array.reduce((a, b) => {
+      //   return a + b;
+      // }, 0);
+      //reduce
+      return {
+        ...state,
+        cartList,
+        total: calculateTotalPrice(cartList),
+      };
+    case "CHANGE_CART_QUANTITY":
+      cartList[index].quantity = action.payload.quantity;
+      return {
+        ...state,
+        cartList,
+        total: calculateTotalPrice(cartList),
+      };
+    case "REMOVE_CART_ITEM":
+      cartList.splice(index, 1);
+      return {
+        ...state,
+        cartList,
+        total: calculateTotalPrice(cartList),
+      };
+    default:
+      return state;
+  }
+};
+
 export const CartContext = createContext({});
