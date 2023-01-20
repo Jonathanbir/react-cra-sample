f0d4fd8f4bf75d934245146a738c8b29bb132b86
Jonathanbir <jonathan79111@gmail.com>
2023-01-21 00:03:54 +0800

    class:搜尋及網址參數

--- src/components/AlbumSearch.js
+++ src/components/AlbumSearch.js
@@ -1,23 +1,36 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function AlbumSearch() {
   const [list, setList] = useState([]);
   const api = "https://api.unsplash.com/search/photos/";
   const accessId = process.env.REACT_APP_UNSPLASH_ACESS;
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  //   useEffect(() => {
+  //     console.log(searchParams.get("query")); //曲出方法
+
+  //     setSearchParams({ query: "building" });
+  //   }, []);
 
   useEffect(() => {
-    (async () => {
-      const response = await axios.get(
-        `${api}?client_id=${accessId}&query=${search}`
-      );
-      const { results } = response.data;
-      console.log(results);
-      setList(results);
-    })();
+    if (search !== "") {
+      (async () => {
+        const response = await axios.get(
+          `${api}?client_id=${accessId}&query=${search}`
+        );
+        const { results } = response.data;
+        console.log(results);
+        setList(results);
+      })();
+    }
   }, [search]);
+
+  useEffect(() => {
+    setSearch(searchParams.get("query"));
+  }, [searchParams]);
 
   return (
     <div>
@@ -28,7 +41,8 @@ export default function AlbumSearch() {
         defaultValue={search}
         onKeyUp={(e) => {
           if (e.code === "Enter") {
-            setSearch(e.target.value);
+            // setSearch(e.target.value);
+            setSearchParams({ query: e.target.value });
           }
         }}
       />
